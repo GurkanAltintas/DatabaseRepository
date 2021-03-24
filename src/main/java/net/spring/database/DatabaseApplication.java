d10eb69ad4827f69a37bf73197081696475e83ba
gurkan <qurkan.a00@gmail.com>
2021-03-24 14:31:34 +0300

    new query type

--- src/main/java/net/spring/database/DatabaseApplication.java
+++ src/main/java/net/spring/database/DatabaseApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.util.List;
+
 @SpringBootApplication
 public class DatabaseApplication {
 
@@ -14,12 +16,15 @@ public class DatabaseApplication {
 
 		IStudentRepository bean = context.getBean(IStudentRepository.class);
 
-		/*Student student=new Student(null,"gurkan","altintas",511L,23L);
+		//Student student=new Student(null,"osman","sakat",511L,23L);
 
-		Student save=bean.save(student);*/
+		//Student save=bean.save(student);
 
 		Student student1= bean.findByName("go");
 		System.out.println(student1);
+
+		Student student2=bean.findStudentByNameSAndSurname("osman","sakat");
+		System.out.println(student2);
 	}
 
 }
--- src/main/java/net/spring/database/repository/IStudentRepository.java
+++ src/main/java/net/spring/database/repository/IStudentRepository.java
@@ -2,9 +2,16 @@ package net.spring.database.repository;
 
 import net.spring.database.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface IStudentRepository extends JpaRepository<Student,Long> {
 
 
     Student findByName(String name);
+    @Query("select s from Student s where s.name = :name and s.surname=:surname")
+    Student findStudentByNameSAndSurname(@Param("name") String name,
+                                         @Param("surname") String surname);
 }
